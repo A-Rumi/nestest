55eb877dda162ca62f35f2f4d07d49a60c1adad8
Rumi <car3@torder.co.kr>
2022-10-28 12:19:27 +0900

    feat: RedisIoAdapter 적용
    Redis Adapter for Socket.Io cluster

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { RedisIoAdapter } from './events/redisIoAdapter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
   await app.listen(3000);
 }
 bootstrap();
